e0468072653e0437f2778f0adbfdbaed7cd61ad9
huyhung1404 <73883857+huyhung1404@users.noreply.github.com>
2022-06-29 20:47:02 +0700

    Update DeveloperDebugEditorWindow.cs

--- DeveloperDebug/Assets/DeveloperDebug/Editor/DeveloperDebugEditorWindow.cs
+++ DeveloperDebug/Assets/DeveloperDebug/Editor/DeveloperDebugEditorWindow.cs
@@ -2,48 +2,53 @@ namespace DeveloperDebug.Editor
 {
     using System;
     using System.Collections.Generic;
-    using DeveloperDebug.Core;
+    using Core;
     using UnityEditor;
     using UnityEngine;
     public class DeveloperDebugEditorWindow : EditorWindow
     {
-        private static DeveloperDebugEditorWindow _window;
-        private string _textCode;
-        private bool _focusTextField;
-        private Vector2 _scrollPos;
-        private List<string> _keyData = new List<string>();
-        private int _keyCount;
+        private static DeveloperDebugEditorWindow m_Window;
+        private string m_TextCode;
+        private bool m_FocusTextField;
+        private Vector2 m_ScrollPos;
+        private DeveloperDebugSetting m_Setting;
+        private Dictionary<string, Action> m_KeyCodeData;
 
         [MenuItem("Window/DeveloperDebug")]
         public static void OpenPopupWindow()
         {
-            _window = GetWindow<DeveloperDebugEditorWindow>("Developer Debug");
-            _window.maxSize = new Vector2(600, 100);
-            _window.minSize = new Vector2(600, 35);
-            _window.Init();
+            m_Window = GetWindow<DeveloperDebugEditorWindow>("Developer Debug");
+            m_Window.maxSize = new Vector2(600, 100);
+            m_Window.minSize = new Vector2(600, 35);
+            m_Window.Init();
         }
 
         private void Init()
         {
-            _textCode = string.Empty;
-            _focusTextField = false;
-            _keyData.Clear();
-            // _keyData = DeveloperDebugKeyCode.GetKeyData();
-            _keyCount = _keyData.Count;
+            m_TextCode = string.Empty;
+            m_FocusTextField = false;
+            m_Setting = Resources.Load<DeveloperDebugSetting>("DeveloperDebugSetting");
+            m_KeyCodeData = m_Setting.GetKeyCodeData();
         }
 
         private void OnGUI()
         {
-            if (_focusTextField)
+            if (m_FocusTextField)
             {
-                _textCode = EditorGUILayout.TextField("Developer Code: ", _textCode);
+                m_TextCode = EditorGUILayout.TextField("Developer Code: ", m_TextCode);
             }
             else
             {
                 GUI.SetNextControlName("textCodeField");
-                _textCode = EditorGUILayout.TextField("Developer Code: ", _textCode);
+                m_TextCode = EditorGUILayout.TextField("Developer Code: ", m_TextCode);
                 EditorGUI.FocusTextInControl("textCodeField");
-                _focusTextField = true;
+                m_FocusTextField = true;
+            }
+            GUI.SetNextControlName("buttonSetting");
+            if (GUILayout.Button("Go To Developer Debug Setting"))
+            {
+                Selection.activeObject = m_Setting;
+                m_Window.Close();
             }
 
             DrawKeyValue();
@@ -51,10 +56,13 @@ namespace DeveloperDebug.Editor
             if (!Event.current.isKey) return;
             switch (Event.current.keyCode)
             {
+                case KeyCode.Tab:
+                    EditorGUI.FocusTextInControl("buttonSetting");
+                    break;
                 case KeyCode.Return:
                 case KeyCode.KeypadEnter:
                     ExecuteDeveloperCode();
-                    _window.Close();
+                    m_Window.Close();
                     Event.current.Use();
                     break;
             }
@@ -62,39 +70,39 @@ namespace DeveloperDebug.Editor
 
         private void DrawKeyValue()
         {
-            EditorGUILayout.LabelField($"Has {_keyCount} key code", EditorStyles.centeredGreyMiniLabel);
-            _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, GUILayout.Width(600), GUILayout.Height(80));
-            var totalRow = (int)Math.Ceiling(_keyCount / 3f);
+            EditorGUILayout.LabelField($"Has {m_KeyCodeData.Count} key code", EditorStyles.centeredGreyMiniLabel);
+            m_ScrollPos = EditorGUILayout.BeginScrollView(m_ScrollPos, GUILayout.Width(600), GUILayout.Height(80));
+            var totalRow = (int)Math.Ceiling(m_KeyCodeData.Count / 3f);
             for (var i = 0; i < totalRow; i++)
             {
                 DrawRow(i);
             }
-
+            
             EditorGUILayout.EndScrollView();
         }
 
         private void DrawRow(int i)
         {
-            EditorGUILayout.BeginHorizontal();
-            var keyIndex = _keyData.Count - 1;
-            EditorGUILayout.LabelField(i * 3 <= keyIndex ? _keyData[i * 3] : "");
-            EditorGUILayout.LabelField(i * 3 + 1 <= keyIndex ? _keyData[i * 3 + 1] : "");
-            EditorGUILayout.LabelField(i * 3 + 2 <= keyIndex ? _keyData[i * 3 + 2] : "");
-            EditorGUILayout.EndHorizontal();
+            // EditorGUILayout.BeginHorizontal();
+            // var keyIndex = _keyData.Count - 1;
+            // EditorGUILayout.LabelField(i * 3 <= keyIndex ? _keyData[i * 3] : "");
+            // EditorGUILayout.LabelField(i * 3 + 1 <= keyIndex ? _keyData[i * 3 + 1] : "");
+            // EditorGUILayout.LabelField(i * 3 + 2 <= keyIndex ? _keyData[i * 3 + 2] : "");
+            // EditorGUILayout.EndHorizontal();
         }
 
         private void ExecuteDeveloperCode()
         {
-            if (string.IsNullOrEmpty(_textCode)) return;
-            // var dic = DeveloperDebugKeyCode.GetData();
-            // if (dic.ContainsKey(_textCode))
-            // {
-            //     dic[_textCode]?.Invoke();
-            //     Debug.Log("Execute");
-            //     return;
-            // }
-
-            Debug.LogError("Key does not exist");
+            // if (string.IsNullOrEmpty(_textCode)) return;
+            // // var dic = DeveloperDebugKeyCode.GetData();
+            // // if (dic.ContainsKey(_textCode))
+            // // {
+            // //     dic[_textCode]?.Invoke();
+            // //     Debug.Log("Execute");
+            // //     return;
+            // // }
+            //
+            // Debug.LogError("Key does not exist");
         }
     }
 }
